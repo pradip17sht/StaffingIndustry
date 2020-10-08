2e0a2b3a80948fc7aa49e5cd70169b0358934c34
pradip17sht <pradip17sht@gmail.com>
2020-10-08 16:37:43 +0545

    crud done in backend for customer

---
+++ StaffingIndustry.Application.Model/Customer/MvCustomer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace StaffingIndustry.Application.Model.Customer
+{
+    public class MvCustomer
+    {
+        [Required]
+        public string organizationName { get; set; }
+
+        [Required]
+        public string city { get; set; }
+
+        [Required]
+        public string state { get; set; }
+
+        [Required]
+        public int phoneNo { get; set; }
+
+        [Required]
+        public string emailId { get; set; }
+    }
+
+    public class MvEditCustomer
+    {
+        [Required]
+        public int organizationId { get; set; }
+        
+        [Required]
+        public string organizationName { get; set; }
+
+        [Required]
+        public string city { get; set; }
+
+        [Required]
+        public string state { get; set; }
+
+        [Required]
+        public int phoneNo { get; set; }
+
+        [Required]
+        public string emailId { get; set; }
+    }
+}
---
+++ StaffingIndustry.Application.Service/Customer/ICustomerService.cs
@@ -0,0 +1,14 @@
+﻿using StaffingIndustry.Application.Model.Customer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaffingIndustry.Application.Service.Customer
+{
+    public interface ICustomerService
+    {
+        dynamic GetAllCustomerDetail();
+        bool AddCustomer(MvCustomer customer);
+        bool EditCustomer(MvEditCustomer customer);
+    }
+}
